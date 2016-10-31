124cc1e8ceb3f487fd4da5ac9eb7e7887380f0bf
Anulfo Ordaz <noaharkling@gmail.com>
2016-10-31 15:42:05 -0500

    Documentation for Cart and Order Controllers (#160)
    * Documanetation for Cart and Order Controllers
    
    * Final Changes

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -17,26 +17,36 @@ namespace BangazonWeb.Controllers
      * Methods:
      *   IActionResult Index() - Queries the Active User and returns that users' Cart view.
      *   Task<IActionResult> AddToCart(int id) - Creates a new line item for the current active order. If no current order, also creates a new order. Returns Product Detail view.
-     *          - int id: ProductId for the Product that needs added to an order.
      *   Task<IActionResult> DeleteLineItem(int id) - Deletes a LineItem from the current active order. Redirects to Cart view.
-     *          - int id: LineItemId for the LineItem that needs to be deleted.
      *   Task<IActionResult> CompleteOrder(OrderView orderView) - Checks whether a PaymentType has been added to the current active order. If so, adds a DateCompleted to the order and saves to DB.
-     *          - OrderView orderView: Order ViewModel posted on submission of the form
      *   Task<IActionResult> Confirmation(int id) - Returns a Confirmation view that lists cart information, the order# and payment method.
-     *          - int id: OrderId for the completed order.
      *   IActionResult Error() - Returns an Error view. Currently not used.
      */
     public class CartController : Controller
     {
         private BangazonContext context;
-        
+
+        /**
+         * Purpose: load the content of BangazonContext into the context variable for further use
+         * Arguments:
+         *      ctx - current database connection
+         * Return:
+         *      CartsController
+         */
+
         public CartController(BangazonContext ctx)
         {
             context = ctx;
         }
 
-
-    /* Adds logged in user and checks view model.  Sends view model to view file. */
+        /**
+         * Purpose: Retrieve products to a list the Cart View. If there are no Objects,
+         it redirects to ProductTypes/Index, sets TotalPrice for the items in the cart.
+         * Arguments:
+         *      None.
+         * Return:
+         *      Model of the Cart Index View
+         */
         public IActionResult Index()
         {
             var model = new CartView(context);
@@ -47,6 +57,7 @@ namespace BangazonWeb.Controllers
                 return RedirectToAction("Index", "ProductTypes");
             }
 
+            //loop to retrieve the total price of the products
             foreach (var product in model.CartProducts)
             {
                 if (product.IsActive)
@@ -58,9 +69,9 @@ namespace BangazonWeb.Controllers
 
         /**
          * Purpose: Adds a product to a user's open order
-         * Arguments: 
-         *      id - product id that creates a line item in an active order 
-         * Return: 
+         * Arguments:
+         *      id - product id that creates a line item in an active order
+         * Return:
          *      Redirects user to product detail view
          */
         public async Task<IActionResult> AddToCart([FromRoute]int id)
@@ -112,16 +123,22 @@ namespace BangazonWeb.Controllers
                      new { controller = "Products", action = "Detail", Id = id } ) );
         }
 
-
+/**
+ * Purpose: Delete a LineItem from an open order
+ * Arguments:
+ *      id - product id taken from the route that deletes an LineItem from open order
+ * Return:
+ *      After the file deletion is complete, it redirects to the Cart/Index view.
+ */
         public async Task<IActionResult> DeleteLineItem([FromRoute]int id)
         {
-
+            // Gets the Open Order based on the ActiveUser
             Order OpenOrder = await(
                 from order in context.Order
                 where order.UserId == ActiveUser.Instance.User.UserId && order.DateCompleted == null
                 select order).SingleOrDefaultAsync();
 
-
+                // Retrieve the LineItem to be deleted
             LineItem deletedItem = await context.LineItem.FirstAsync(p => p.ProductId == id && p.OrderId == OpenOrder.OrderId);
 
 
@@ -143,16 +160,24 @@ namespace BangazonWeb.Controllers
                 throw;
             }
         }
-
+/**
+ * Purpose: Get the open order, set the payment type if the user already has one and redirectes to
+    confirmation view.
+ * Arguments:
+ *      OrderView orderView. When the payment has been selected gives the openOrder the the PaymentTypeId
+ * Return:
+ *      Redirects user to product detail view
+ */
         public async Task<IActionResult> CompleteOrder(OrderView orderView)
         {
+            //Instanciate an ActiveUser into a User user
             User user = ActiveUser.Instance.User;
             int? userId = user.UserId;
             if (userId == null)
             {
                 return Redirect("ProductTypes");
             }
-
+            //get the open order
             Order openOrder = await(
                 from order in context.Order
                 where order.UserId == userId && order.DateCompleted == null
@@ -174,6 +199,7 @@ namespace BangazonWeb.Controllers
 
             try
             {
+                //redirects to confirmation page
                 openOrder.DateCompleted = DateTime.Now;
                 context.Order.Update(openOrder);
                 await context.SaveChangesAsync();
@@ -186,9 +212,18 @@ namespace BangazonWeb.Controllers
                 throw;
             }
         }
+
+        /**
+         * Purpose: get the payment type, the lineItems, and the complete order into CartView model to be displayed
+         by Confirmation page.
+         * Arguments:
+         *      int id - Represents the openOrder.OrderId
+         * Return:
+         *      Order confirmation view if an open order exists
+         */
         public async Task<IActionResult> Confirmation(int id)
         {
-
+            //get the complete order
             Order CompleteOrder = await(
                 from order in context.Order
                 where order.OrderId == id
@@ -199,20 +234,14 @@ namespace BangazonWeb.Controllers
                 return RedirectToAction("Buy", "ProductTypes");
             }
 
-            if (CompleteOrder.UserId != CompleteOrder.UserId)
-            {
-                return Redirect("ProductTypes");
-            }
-
-
-
+            //get the line items for the order
             var LineItems = await(
                 from product in context.Product
                 from lineItem in context.LineItem
                     .Where(lineItem => lineItem.OrderId == id && lineItem.ProductId == product.ProductId && lineItem.Product.IsActive == true)
                 select product).ToListAsync();
 
-
+            //Instanciation of a New CartView Model
             var model = new CartView(context);
 
             model.PaymentType = await context.PaymentType.SingleAsync(t => t.PaymentTypeId == CompleteOrder.PaymentTypeId);
@@ -226,11 +255,6 @@ namespace BangazonWeb.Controllers
             }
 
             return View(model);
-        }
-
-        public IActionResult Error()
-        {
-            return View();
         }
     }
 }
