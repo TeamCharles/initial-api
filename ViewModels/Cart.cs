1605e179cfb54a85daeb2289a5ee5810683165fb
Matt Hamil <j.matthew.hamil@gmail.com>
2016-10-31 14:28:09 -0500

    Added documentation

--- ViewModels/Cart.cs
+++ ViewModels/Cart.cs
@@ -6,6 +6,13 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BangazonWeb.ViewModels
 {
+  /**
+   * Class: CartView
+   * Purpose: Used when displaying a user's cart
+   * Author: Garrett Vangilder
+   * Methods:
+   *   Constructor CartView(ctx) - Initiates a cart view model with a reference to the database context.
+   */
   public class CartView : BaseViewModel
   {
     public decimal TotalPrice { get; set; }
@@ -13,6 +20,11 @@ namespace BangazonWeb.ViewModels
     public PaymentType PaymentType { get; set; }
     public IEnumerable<Product> LineItems { get; set; }
 
+    /**
+     * Purpose: Saves the reference to the database to be used to display the list of products on the active order
+     * Arguments:
+     *      ctx - Database context reference
+     */
     public CartView(BangazonContext ctx) : base(ctx) { }
     public CartView() { }
   }
